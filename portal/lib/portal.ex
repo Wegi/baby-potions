cb70f5d3307a88c916b8ce8c06e19f79c9a05824
Alexander Schneider <alexander@schneider.gg>
2016-04-06 12:49:55 +0200

    add transfer function to portal module

--- portal/lib/portal.ex
+++ portal/lib/portal.ex
@@ -1,4 +1,7 @@
 defmodule Portal do
+
+  defstruct [:left, :right]
+
   use Application
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
@@ -15,5 +18,33 @@ defmodule Portal do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  Starts transferting ´data´ from ´left´ to ´right´.
+  """
+  def transfer(left, right, data) do
+    # First add all data to the portal on the left
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    # Returns a portal struct we will use next
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data to the right in the given 'portal'.
+  """
+  def push_right(portal) do
+    # See if we can pop data from left. If so, push the
+    # popped data to the right. Otherwise, do nothing.
+    case Portal.Door.pop(portal.left, h) do
+      :error    -> :ok
+      {:ok, h}  -> Portal.Door.push(portal.right, h)
+    end
+
+    # Return the portal
+    portal
   end
 end
