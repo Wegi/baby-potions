2f5dff7082e436b35e7c0fda356cf0b2ff2510b4
Alexander Schneider <alexander@schneider.gg>
2016-04-07 13:07:24 +0200

    add proper representation through Inspect Protocol

--- portal/lib/portal.ex
+++ portal/lib/portal.ex
@@ -39,12 +39,31 @@ defmodule Portal do
   def push_right(portal) do
     # See if we can pop data from left. If so, push the
     # popped data to the right. Otherwise, do nothing.
-    case Portal.Door.pop(portal.left, h) do
+    case Portal.Door.pop(portal.left) do
       :error    -> :ok
       {:ok, h}  -> Portal.Door.push(portal.right, h)
     end
 
     # Return the portal
     portal
+  end
+end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door = inspect(left)
+    right_door = inspect(right)
+
+    left_data = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
   end
 end
