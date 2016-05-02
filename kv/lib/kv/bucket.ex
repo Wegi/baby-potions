d1d712043d36fc7329788495feaa302ec0d83e57
Alexander Schneider <alexander@schneider.gg>
2016-05-02 23:09:37 +0200

    experiment with Agents

---
+++ kv/lib/kv/bucket.ex
@@ -0,0 +1,31 @@
+defmodule KV.Bucket do
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Put a new `value` for the `key` in the
+  `bucket`
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+  Get the `value` for a `key` from the `bucket`
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Delete and deliver the value to a `key` from a bucket
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key)) 
+  end
+end
